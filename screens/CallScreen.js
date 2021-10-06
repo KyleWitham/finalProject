351dc04ce2166c8a0ea805d3f8f21f2876f9b4f3
Rudy DeSanti <rudysd3@gmail.com>
2021-10-06 19:28:30 -0400

    final commit for now

--- screens/CallScreen.js
+++ screens/CallScreen.js
@@ -35,7 +35,7 @@ export default function CallScreen({navigation, ...props}) {
   //This could be the cloud server
   //const [conn, setConn] = useState(new WebSocket('ws://10.80.105.54:3000'));
 
-  const [conn, setConn] = useState(io('ws://10.80.105.54:3000'));
+  const [conn, setConn] = useState(io('ws://10.80.106.24:3000'));
   //console.log(conn.id);
   //const conn = io('ws://10.80.105.54:3000');
 
@@ -102,6 +102,7 @@ export default function CallScreen({navigation, ...props}) {
       console.log(InCallManager);
 
       console.log("Connecting to server Login");
+      //conn.id = userId;
       send({
         type: 'login',
         name: userId,
